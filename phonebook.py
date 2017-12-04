d76522573fdb412a993fa168786ddd9d57b9981c
Vishnu Poothery <vishnupoothery@outlook.com>
2017-12-04 21:02:48 +0530

    created phonebook.py and contacts.data

---
+++ phonebook.py
@@ -0,0 +1,14 @@
+print ("\t\tPeople Hub\n")
+
+cmd = ""
+
+while cmd != "0":
+	print ("Enter your choice")
+	print ("1. Add contacts")
+	print ("2. Search Contact")
+	print ("0. Exit")
+	cmd = input()
+	if (cmd == "1"):
+		print ("Add Contacts")
+	elif (cmd == "2"):
+		print ("Search Contacts")
