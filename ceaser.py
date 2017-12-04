0ec33c2c75887d0087d885c40e1291d7995945f7
Vishnu Poothery <vishnupoothery@outlook.com>
2017-12-04 19:18:09 +0530

    added encryption function

--- ceaser.py
+++ ceaser.py
@@ -3,13 +3,30 @@ print ("Welcome to Ceaser's Cipher\n")
 cmd = ""
 
 while cmd !="0":
-    print ("Enter your choice")
-    print ("1. Encrypt")
-    print ("2. Decrypt")
-    print ("0. Exit")
+	print ("Enter your choice")
+	print ("1. Encrypt")
+	print ("2. Decrypt")
+	print ("0. Exit")
 
-    cmd = input(" : ")
+	cmd = input(" : ")
 
-    if cmd == "1":
-        string = input("Enter string to Encrypt :\n")
-        key = int(input("Enter encryption key : "))
+	if cmd == "1":
+		string = input("Enter string to Encrypt :\n")
+		key = int(input("Enter encryption key : "))
+		key=key%26
+		enc=""
+		for i in range(len(string)):
+			val = ord(string[i])
+			if(val >=ord('a') and val <=ord('z')):
+				val-=ord('a')
+				val+=key;
+				val = val%26;
+				val+=ord('a')
+			elif(val >=ord('A') and val <=ord('Z')):
+				val-=ord('A')
+				val+=key;
+				val = val%26;
+				val+=ord('A')
+			enc=enc+chr(val)
+		print (enc)
+	
